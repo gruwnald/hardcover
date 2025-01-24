b39234500483ee5181e495806844a680287f674a
Krzysztof Mizgała <km.2001@o2.pl>
2025-01-24 12:49:00 +0100

    Add gradient boosting regression model
    enhance user input for feature selection

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,16 +1,29 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.multioutput import MultiOutputRegressor
 
-"""
-# Modele predykcyjne
-## Regresja liniowa
-"""
+"# Modele predykcyjne"
 
 df = st.session_state.df
+
+columns = st.multiselect('Wybierz kolumny do modelowania', 
+                         ['ratings_count', 'release_year',
+                          'pages', 'editions_count', 'lists_count',
+                          'journals_count', 'reviews_count', 'Genre',
+                          'Mood', 'Content Warning', 'Tag'],
+                         default=['ratings_count', 'release_year',
+                                  'pages', 'editions_count', 'lists_count',
+                                  'journals_count', 'reviews_count', 'Genre',
+                                  'Mood', 'Content Warning', 'Tag'])
+if not columns:
+    st.write("Wybierz co najmniej jedną kolumnę")
+    st.stop()
 
 @st.cache_resource(hash_funcs={pd.DataFrame: id})
 def prepare_data(df):
@@ -32,14 +45,9 @@ def prepare_data(df):
             count * (rating - x.rating) ** 2 
             for rating, count in x.ratings.items()
         ) / (x.ratings_count - 1), axis=1)
-    return df.drop(columns=["ratings"]), top_values
+    df = df.drop(columns=["ratings"])
 
-df, top_values = prepare_data(df)
-
-@st.cache_resource(hash_funcs={pd.DataFrame: id, list: id})
-def linear_regression(df, columns):
     df = df[['rating', 'rating_var'] + columns]
-
     mlb = MultiLabelBinarizer()
     for column in top_values.keys():
         if column in columns:
@@ -49,56 +57,53 @@ def linear_regression(df, columns):
     X = df.drop(columns=['rating', 'rating_var'], axis=1).astype("float32").to_numpy()
     y = df[['rating', 'rating_var']].astype("float32").to_numpy()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=314)
+    return X_train, X_test, y_train, y_test, df.columns.drop(['rating', 'rating_var'])
 
+X_train, X_test, y_train, y_test, train_columns = prepare_data(df)
+
+"## Regresja liniowa"
+
+@st.cache_resource(hash_funcs={np.ndarray: id})
+def linear_regression(X_train, X_test, y_train, y_test):
     model_rating = LinearRegression()
     model_rating.fit(X_train, y_train[:, 0])
 
     model_rating_var = LinearRegression()
     model_rating_var.fit(X_train, y_train[:, 1])
-    return df, model_rating, model_rating_var, (X_train, X_test, y_train, y_test)
 
-columns = st.multiselect('Wybierz kolumny do modelowania', 
-                         ['ratings_count', 'release_year',
-                          'pages', 'editions_count', 'lists_count',
-                          'journals_count', 'reviews_count', 'Genre',
-                          'Mood', 'Content Warning', 'Tag'],
-                         default=['ratings_count', 'release_year',
-                                  'pages', 'editions_count', 'lists_count',
-                                  'journals_count', 'reviews_count', 'Genre',
-                                  'Mood', 'Content Warning', 'Tag'])
-if not columns:
-    st.write("Wybierz co najmniej jedną kolumnę")
-    st.stop()
+    y_pred_rating = model_rating.predict(X_test)
+    mse_rating = mean_squared_error(y_test[:, 0], y_pred_rating)
+    y_pred_rating_var = model_rating_var.predict(X_test)
+    mse_rating_var = mean_squared_error(y_test[:, 1], y_pred_rating_var)
 
-df, model_rating, model_rating_var, train_test = linear_regression(df, columns)
-X_train, X_test, y_train, y_test = train_test
+    def sum_coefficients(coef, numeric_cols, columns):
+        coef_sum = {}
+        for column in ('Genre', 'Mood', 'Content Warning', 'Tag'):
+            if column in columns:
+                coef_sum[column] = coef.filter(like=column).sum()
+        for column in numeric_cols:
+            coef_sum[column] = coef[column]
+        return pd.Series(coef_sum)
 
-y_pred_rating = model_rating.predict(X_test)
-mse_rating = mean_squared_error(y_test[:, 0], y_pred_rating)
-y_pred_rating_var = model_rating_var.predict(X_test)
-mse_rating_var = mean_squared_error(y_test[:, 1], y_pred_rating_var)
-st.write(f"Błąd średniokwadratowy dla średniej oceny: {mse_rating:.4f}")
-st.write(f"Błąd średniokwadratowy dla wariancji ocen: {mse_rating_var:.4f}")
+    coef_rating = pd.Series(model_rating.coef_, index=train_columns)
+    coef_rating_var = pd.Series(model_rating_var.coef_, index=train_columns)
+    numeric_cols = ['release_year', 'pages', 'editions_count', 'lists_count', 'journals_count', 'reviews_count']
+    numeric_cols = [col for col in numeric_cols if col in columns]
+    coef_sum_rating = sum_coefficients(coef_rating, numeric_cols, columns).sort_values(ascending=False)
+    coef_sum_rating_var = sum_coefficients(coef_rating_var, numeric_cols, columns).sort_values(ascending=False)
+    coef_sum_rating.name = 'Współczynnik'
+    coef_sum_rating_var.name = 'Współczynnik'
 
-def sum_coefficients(coef, top_values, numeric_cols, columns):
-    coef_sum = {}
-    for column in top_values.keys():
-        if column in columns:
-            coef_sum[column] = coef.filter(like=column).sum()
-    for column in numeric_cols:
-        coef_sum[column] = coef[column]
-    return pd.Series(coef_sum)
+    return mse_rating, mse_rating_var, coef_sum_rating, coef_sum_rating_var
 
-coef_rating = pd.Series(model_rating.coef_, index=df.drop(columns=['rating', 'rating_var']).columns)
-coef_rating_var = pd.Series(model_rating_var.coef_, index=df.drop(columns=['rating', 'rating_var']).columns)
-numeric_cols = ['release_year', 'pages', 'editions_count', 'lists_count', 'journals_count', 'reviews_count']
-numeric_cols = [col for col in numeric_cols if col in columns]
-coef_sum_rating = sum_coefficients(coef_rating, top_values, numeric_cols, columns).sort_values(ascending=False)
-coef_sum_rating_var = sum_coefficients(coef_rating_var, top_values, numeric_cols, columns).sort_values(ascending=False)
-coef_sum_rating.name = 'Współczynnik'
-coef_sum_rating_var.name = 'Współczynnik'
+mse_rating, mse_rating_var, coef_sum_rating, coef_sum_rating_var = linear_regression(X_train, X_test, y_train, y_test)
 
-"### Współczynniki regresji"
+st.write(f"""
+Błąd średniokwadratowy dla średniej oceny: **{mse_rating:.4f}**  
+Błąd średniokwadratowy dla wariancji ocen: **{mse_rating_var:.4f}**
+""")
+
+"**Współczynniki regresji:**"
 col1, col2 = st.columns(2)
 with col1:
     "Współczynniki dla średniej oceny"
@@ -106,3 +111,20 @@ with col1:
 with col2:
     "Współczynniki dla wariancji ocen"
     st.write(coef_sum_rating_var)
+
+"## Gradient Boosting"
+@st.cache_resource(hash_funcs={np.ndarray: id})
+def gradient_boosting(X_train, X_test, y_train, y_test):
+    model = MultiOutputRegressor(GradientBoostingRegressor())
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    mse_rating = mean_squared_error(y_test[:, 0], y_pred[:, 0])
+    mse_rating_var = mean_squared_error(y_test[:, 1], y_pred[:, 1])
+    return mse_rating, mse_rating_var
+
+mse_rating, mse_rating_var = gradient_boosting(X_train, X_test, y_train, y_test)
+
+st.write(f"""
+Błąd średniokwadratowy dla średniej oceny: **{mse_rating:.4f}**  
+Błąd średniokwadratowy dla wariancji ocen: **{mse_rating_var:.4f}**
+""")
