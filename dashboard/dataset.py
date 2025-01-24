2ebbffe4012f4eb3b5109487927de3496f6b9e0f
Krzysztof Mizgała <km.2001@o2.pl>
2025-01-24 13:10:51 +0100

    Fix rating_var in dataset.py

--- dashboard/dataset.py
+++ dashboard/dataset.py
@@ -6,6 +6,14 @@ import streamlit as st
 """
 
 df = st.session_state.df
+
+df['rating_var'] = df.apply(
+    lambda x: sum(
+        count * (rating - x.rating) ** 2 
+        for rating, count in x.ratings.items()
+    ) / (x.ratings_count - 1), axis=1)
+df = df.drop(columns=["ratings"])
+
 col1, col2 = st.columns(2)
 with col1:
     rating_filter = st.slider('Średnia ocena',
@@ -21,34 +29,30 @@ with col1:
                                max_value=int(df['release_year'].max()),
                                value=int(df['release_year'].max()))
     count_filter = st.slider('Liczba ocen',
-                                    min_value=0,
-                                    max_value=df.ratings_count.max(),
-                                    value=(0, df.ratings_count.max()))
+                             min_value=0,
+                             max_value=df.ratings_count.max(),
+                             value=(0, df.ratings_count.max()))
 
 with col2:
-    std_filter = st.slider('Odchylenie standardowe',
-                            min_value=0,
-                            max_value=5,
-                            value=(0, 5))
-#     std_filter = st.slider('Odchylenie standardowe',
-#                            min_value=df.std_rating.min(),
-#                            max_value=df.std_rating.max(),
-#                            value=(df.std_rating.min(), df.std_rating.max()))
+    std_filter = st.slider('Wariancja ocen',
+                           min_value=df.rating_var.min(),
+                           max_value=df.rating_var.max(),
+                           value=(df.rating_var.min(), df.rating_var.max()))
     min_pages = st.number_input('Minimalna liczba stron',
-                                  min_value=int(df['pages'].min()),
-                                  max_value=int(df['pages'].max()),
-                                  value=int(df['pages'].min()))
+                                min_value=int(df['pages'].min()),
+                                max_value=int(df['pages'].max()),
+                                value=int(df['pages'].min()))
     max_pages = st.number_input('Maksymalna liczba stron',
-                                  min_value=int(df['pages'].min()),
-                                  max_value=int(df['pages'].max()),
-                                  value=int(df['pages'].max()))
+                                min_value=int(df['pages'].min()),
+                                max_value=int(df['pages'].max()),
+                                value=int(df['pages'].max()))
     title_filter = st.text_input('Szukaj tytułu książki', '')
 
 genre_filter = st.multiselect('Wybierz gatunki', df['Genre'].explode().unique(), default=('Fantasy'))
 
 filtered_df = df[
     (df['rating'] >= rating_filter[0]) & (df['rating'] <= rating_filter[1]) &
-    # (df['std_rating'] >= std_filter[0]) & (df['std_rating'] <= std_filter[1]) &
+    (df['rating_var'] >= std_filter[0]) & (df['rating_var'] <= std_filter[1]) &
     (df['ratings_count'] >= count_filter[0]) & (df['ratings_count'] <= count_filter[1]) &
     (df['pages'] >= min_pages) & (df['pages'] <= max_pages) &
     (df['release_year'] >= min_year) & (df['release_year'] <= max_year) &
@@ -58,6 +62,6 @@ filtered_df = df[
 
 cols = df.columns
 selected_cols = st.multiselect('Wybierz kolumny do wyświetlenia', cols,
-    default=['title', 'rating', 'ratings_count', 'pages', 'users_count', 'Genre'])
+    default=['title', 'rating', 'rating_var', 'ratings_count', 'pages', 'users_count', 'Genre'])
 
-st.write(filtered_df[selected_cols])
+filtered_df[selected_cols]
